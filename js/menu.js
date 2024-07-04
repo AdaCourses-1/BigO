7bb9ba3d192da3a73363be816bad6a5aa8914e70
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-07-05 02:32:53 +0600

    little style fix for menu animation

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,6 @@ const mobileNav = document.querySelector(".mobile-nav");
 
 mobileMenu?.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
-  mobileNav.classList.toggle('active')
+  mobileNav.classList.toggle('active');
 });
 
