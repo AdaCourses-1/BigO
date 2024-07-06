30ae755c5f463e9015811113fc9e8250bb5a07b3
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-07-06 21:34:20 +0600

    added node builders for js and img files

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 const pug = require('./nodeScripts/pug-compiler.js');
 const scss = require('./nodeScripts/scss-compiler.js')
+const js = require('./nodeScripts/js-builder.js');
+const img = require('./nodeScripts/img-builder.js');
 
 pug.startCompile();
 scss.startCompile();
+
+js.startBuilding();
+img.buildImgFiles();
