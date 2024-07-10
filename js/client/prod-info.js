53809be02d6deadcb09e7101a384653cba687b79
Namelesss01 <zajloobekovali00@gmail.com>
2024-07-11 00:07:48 +0600

    tochno last commit

--- js/client/prod-info.js
+++ js/client/prod-info.js
@@ -1,13 +1,8 @@
 function toggleBlock(id) {
-  // Находим все блоки с классом "block"
   let blocks = document.querySelectorAll(".block");
-
-  // Удаляем класс "active" у всех блоков
   blocks.forEach(function (block) {
     block.classList.remove("active");
   });
-
-  // Находим выбранный блок по его id и добавляем ему класс "active"
   let selectedBlock = document.getElementById(id);
   if (selectedBlock) {
     selectedBlock.classList.add("active");
@@ -15,13 +10,10 @@ function toggleBlock(id) {
 }
 
 const buttons = document.querySelectorAll(".prod-info__btn");
-
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
     buttons.forEach((btn) => btn.classList.remove("active"));
     button.classList.add("active");
-
-    // Вызывайте вашу функцию toggleBlock с соответствующим id блока
     switch (index) {
       case 0:
         toggleBlock("block1");
