ecf37f23d8fcdd32f4f0c42e0ef59d63797dfa62
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-07-11 19:03:35 +0600

    code refactoring

--- public/js/prod-info.js
+++ public/js/prod-info.js
@@ -1,31 +1,25 @@
 function toggleBlock(id) {
   let blocks = document.querySelectorAll(".block");
-  blocks.forEach(function (block) {
+
+  blocks.forEach((block) => {
     block.classList.remove("active");
   });
+
   let selectedBlock = document.getElementById(id);
+
   if (selectedBlock) {
     selectedBlock.classList.add("active");
   }
 }
 
 const buttons = document.querySelectorAll(".prod-info__btn");
+
 buttons.forEach((button, index) => {
   button.addEventListener("click", () => {
     buttons.forEach((btn) => btn.classList.remove("active"));
+
     button.classList.add("active");
-    switch (index) {
-      case 0:
-        toggleBlock("block1");
-        break;
-      case 1:
-        toggleBlock("block2");
-        break;
-      case 2:
-        toggleBlock("block3");
-        break;
-      default:
-        break;
-    }
+
+    toggleBlock(`block${index + 1}`);
   });
 });
