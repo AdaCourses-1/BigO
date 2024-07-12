61356ed9a53009f30d77ce0b879f17ee9f55fbe7
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-07-12 17:37:23 +0600

    cart items issues fix

--- js/client/api/cart-items.js
+++ js/client/api/cart-items.js
@@ -1,53 +1,36 @@
-let cartItems = [
-  {
-    id: 1,
-    name: "Asgaard sofa",
-    price: 49900,
-    count: 2,
-    imageUrl:
-      "https://c0.klipartz.com/pngpicture/669/62/gratis-png-sofa-muebles-salon-minimalismo-viento-nordico-material-de-tres-sofas.png",
-  },
-  {
-    id: 2,
-    name: "Али Софа",
-    price: 19900,
-    count: 3,
-    imageUrl:
-      "https://c0.klipartz.com/pngpicture/867/598/gratis-png-sofa-de-sofa-sofa-cama-publicidad-exterior-material-del-sofa.png",
-  },
-  {
-    id: 3,
-    name: "Олег Шкафостайл",
-    price: 9900,
-    count: 19,
-    imageUrl:
-      "https://www.pngall.com/wp-content/uploads/12/Wardrobe-Furniture-PNG-Images-HD.png",
-  },
-]; // Наши товары которые будут показывается в корзине
+export let cartItems = []; // Наши товары которые будут показывается в корзине
 
+const cartModalIcons = document.querySelectorAll(".cart-modal-icon");
 const cartWrapper = document.getElementById("cart-modal__items"); // ГДЕ, в каком блоке появятся товары
 
-cartItems.forEach((item) => {
-  // Проходимся по всем товарам
-  const cartItem = document.createElement("div"); // Создаем новый пустой div
+function renderCartItems() {
+  cartWrapper.innerHTML = ``;
 
-  cartItem.className = "cart-modal__item"; // Даём этому div класс cart-modal__item
-  cartItem.id = item.id; // Даём этому div его уникальное id
+  cartItems.forEach((item) => {
+    // Проходимся по всем товарам
+    const cartItem = document.createElement("div"); // Создаем новый пустой div
 
-  // Вносим в этот div верстку товара + делаем интерполяцию строк
-  cartItem.innerHTML = `<img class="cart-modal__image" src="${item.imageUrl}" alt="${item.name}">
-                        <div class="cart-modal__info">
-                            <h4 class="cart-modal__subtitle">${item.name}</h4>
-                            <div class="cart-modal__details"> <span class="cart-modal__count">${item.count}</span><span
-                                class="cart-modal__multiply">X</span><span class="cart-modal__price">${item.price} ⃀</span></div>
-                        </div><img class="cart-modal__close" src="./img/close.svg" alt="">`;
+    cartItem.className = "cart-modal__item"; // Даём этому div класс cart-modal__item
+    cartItem.id = item.id; // Даём этому div его уникальное id
 
-  cartWrapper.innerHTML += cartItem.outerHTML; // Создаем товар в html внутри блока cartWrapper
-});
+    // Вносим в этот div верстку товара + делаем интерполяцию строк
+    cartItem.innerHTML = `<img class="cart-modal__image" src="${item.imageUrl}" alt="${item.name}">
+                          <div class="cart-modal__info">
+                              <h4 class="cart-modal__subtitle">${item.name}</h4>
+                              <div class="cart-modal__details"> <span class="cart-modal__count">${item.count}</span><span
+                                  class="cart-modal__multiply">X</span><span class="cart-modal__price">${item.price} ⃀</span></div>
+                          </div><img class="cart-modal__close" src="./img/close.svg" alt="">`;
+
+    cartWrapper.innerHTML += cartItem.outerHTML; // Создаем товар в html внутри блока cartWrapper
+  });
+}
 
 // Даная функция вычесляет итоговую сумму товаров
 function getTotalSum() {
-  const totalPrice = cartItems.reduce((sum, item) => sum + item.price, 0); // Проходимся циклом и собираем все поля price в итоговую сумму
+  const totalPrice = cartItems.reduce(
+    (sum, item) => sum + item.price * item.count,
+    0
+  ); // Проходимся циклом и собираем все поля price в итоговую сумму
   const totalPriceBlock = document.querySelector(".cart-modal__sum"); // Выбираем в каком элемент его показывать в HTML
 
   totalPriceBlock.innerHTML = `${totalPrice} ⃀`; // рисуем итоговую цену в сомах
@@ -82,8 +65,10 @@ function setRemoveActions() {
   });
 }
 
-setRemoveActions();
-getTotalSum();
-
-
-
+cartModalIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    renderCartItems();
+    setRemoveActions();
+    getTotalSum();
+  });
+});
